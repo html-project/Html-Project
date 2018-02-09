343b77613d7703db436b084298808a3befcbfc1b
Zixuan Wang <wzixuanCC@hotmail.com>
2018-02-08 19:56:17 -0500

    Intial commit

---
+++ javascript/node-field.js
@@ -0,0 +1,12 @@
+var classing=class{
+  constructor(){
+    valueMinimizing()
+    if (this.function != ""){
+      return this.function
+    }
+  }
+  valueMinimizing(){
+    this.function="This is a function"
+  }
+}
+console.log(classing)
---
+++ javascript/node-master.js
@@ -0,0 +1,62 @@
+function field(fields){
+  if (fields != ""){
+    return fields
+  } else {
+    var field=class{
+      constructor(){
+        var fieldConstructor="Logs"
+      }
+    }
+    console.log(field)
+    if (field != fields){
+      return field
+    }else{
+      return fields+":"+field
+    }
+  }
+}
+var argumentsOfDefintion=field(fields)
+var fields = class{
+  constructor(){
+    var onlyField=fieldPlus("Logs")
+    fieldMinus(onlyField)
+  }
+  fieldPlus(masterField){
+    if (!this.numberField){
+      var logger=masterField
+      this.numberField=logger
+    }
+    return this.numberField
+  }
+  fieldMinus(field){
+    this.numberField=field
+  }
+}
+console.log(fields)
+var getMaster = class{
+  constructor(){
+    var plus="This is a property"
+    var field=master
+    var master=plus+field
+    master(field, master, plus)
+  }
+  setPlus(plus){
+    this.plus=plus
+  }
+  getMaster(master){
+    if (!this.master){
+      if (master==this.field){
+        this.master=this.field
+      }else{
+        this.master=master
+      }
+    }
+    return this.master
+  }
+  master(field, master, plus){
+    this.field=field
+    this.master=getMaster(master)
+    this.plus=setPlus(plus)
+  }
+}
+console.log(getMaster)
