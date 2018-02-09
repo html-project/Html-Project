3cac07e9d3b54bfc91a539148d930fa2634a2056
Zixuan Wang <wzixuanCC@hotmail.com>
2018-02-08 20:22:50 -0500

    Still need projects from .java file.

---
+++ projects/conflicts.java
@@ -0,0 +1,9 @@
+import java.util.Vector;
+public class conflicts{
+    private static Vector<Integer> stillString;
+    public conflicts(){
+	stillString.addElement(1);
+	stillString.addElement(2);
+	stillString.addElement(3);
+    }
+}
