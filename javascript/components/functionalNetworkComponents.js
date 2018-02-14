d70a7c67efb43a4d8cbc8b1fd57f49670f2228cf
Zixuan Wang <wzixuanCC@outlook.com>
2018-02-14 00:41:43 -0500

    Added javascript file.
    Changed to main component and add some network component description.

---
+++ javascript/components/functionalNetworkComponents.js
@@ -0,0 +1,13 @@
+var functioningDate={
+  propertyValueDefine: function(bar){
+    this.bar=bar
+    console.log(bar)
+  }
+}
+functioningDate.propertyValueDefine(
+  class structure{
+    constructor(){
+      this.syntax="Throws SyntaxError"
+    }
+  }
+)
---
+++ javascript/components/mainComponents.js
@@ -0,0 +1,11 @@
+class EbbedDate{
+  constructor(datingFunction){
+    this.datingFunction=datingFunction
+  }
+}
+var ebbedFunction={
+  phrases: function(){
+    var functions="1007-00-88"
+    var EbbedDate=new EbbedDate(functions)
+  }
+}
