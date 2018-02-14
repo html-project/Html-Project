ed35d0c47dc0a382b4de2c13429cb890f039e55a
Zixuan Wang <33987311+zixuan75@users.noreply.github.com>
2018-02-14 01:01:14 -0500

    Update deleteConflicts.java

--- projects/deleteConflicts.java
+++ projects/deleteConflicts.java
@@ -2,6 +2,8 @@ import java.util.Vector;
 class deleteConflicts{
   public static Vector<int> vectorVec;
   deleteConflicts(){
+    Vector<int> helloString=new Vector<int>();
     vectorVec=new Vector<int>();
+    vectorVec.add(helloString);
   }
 }
