3d50617c1fdb5b6e982ff579af8ea0dd1ee7c912
Zixuan Wang <wzixuanCC@outlook.com>
2018-02-14 14:33:44 -0500

    Change combine rebases.

--- javascript/components/functionalNetworkComponents.js
+++ javascript/components/functionalNetworkComponents.js
@@ -1,4 +1,16 @@
 var functioningDate={
+  combineValue: function(syntax){
+    this.syntax=syntax
+    var functionsToDeclare=class{
+      constructor(){
+        this.value=syntax
+      }
+    }
+    var contents=[1]
+    contents.forEach(content =>{
+      console.log(functionsToDeclare)
+    })
+  },
   propertyValueDefine: function(bar){
     this.bar=bar
     console.log(bar)
@@ -8,6 +20,7 @@ functioningDate.propertyValueDefine(
   class structure{
     constructor(){
       this.syntax="Throws SyntaxError"
+      functioningDate.combineValue(this.syntax)
     }
   }
 )
