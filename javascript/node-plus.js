bc294cb71497e412d8447bdeffa90fb3f594da4c
Zixuan Wang <wzixuanCC@outlook.com>
2018-02-13 21:21:18 -0500

    Change garble description.
    Typo errors fixed, but it changes an garble description of the talk road page. So it is not possible to change that, except the new class changing action, and that is why we do that by telling it.

--- javascript/node-plus.js
+++ javascript/node-plus.js
@@ -4,13 +4,14 @@ var plus=class{
       return "waring"
     }
   }
-    waring(){
-      var masterWaring=this.setPlus(this.plus)
-      var waring=this.getPlus()
-      this.garble=masterWaring
-      this.waring=waring
-    }
+  waring(){
+    var masterWaring=this.setPlus(this.plus)
+    var waring=this.getPlus()
+    this.garble=masterWaring
+    this.waring=waring
+    this.toPlusSet=this.waring+this.garble
   }
+}
 function callPlus(){
   console.log(plus)
 }
