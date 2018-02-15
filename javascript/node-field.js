b1697a12f41e02cb387de1a207731be41e51656d
Zixuan Wang <wzixuanCC@outlook.com>
2018-02-14 21:26:17 -0500

    Changed commited to stations of the communication.

--- javascript/node-field.js
+++ javascript/node-field.js
@@ -17,3 +17,12 @@ function addSomething(){
   }
 }
 addSomething()
+var projects={
+  setStations: function(station){
+    this.stations=station
+  },
+  getStations: function(){
+    return this.stations
+  }
+}
+projects.setStations("1, 2, 3, 4")
