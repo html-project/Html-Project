5e1646caf10c89d5d0401c3d24aa0aa738d4947e
Zixuan Wang <wzixuanCC@outlook.com>
2018-02-17 15:49:06 -0500

    Change components.

--- javascript/components/jsx/NetworkComponents.jsx
+++ javascript/components/jsx/NetworkComponents.jsx
@@ -1,9 +1,16 @@
-function meteorStart(hello){
-  console.log(hello)
+Components=React.getClass(){
+  getProperty(contents){
+    contents.setFooter(contents.footer)
+    contents.setContents(contents.content)
+    contents.setHeader(contents.header)
+  }
+  setComponents(){
+    var classComponents=getProperty(
+      return {
+        header: <Header/>
+        content: "This is a property"
+        footer: <Footer/>
+      }
+    )
+  }
 }
-meteorStart(
-  return(
-    <br>{{results.id}}</br>
-    <img>{{results.url}}</img>
-  )
-)
