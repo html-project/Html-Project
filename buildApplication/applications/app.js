9035811118a64ed735cfeaf2afeab413dd42131f
Zixuan Wang <wzixuanCC@gmail.com>
2018-02-25 18:49:16 -0500

    Description does not enough

--- buildApplication/applications/app.js
+++ buildApplication/applications/app.js
@@ -2,7 +2,7 @@ var logs={
   write: function(){
     var stations=[1, 2, 3]
     stations.forEach(content => {
-      console.log({this.converttoAccessor()}, "My first commit"+null)
+      console.log({this.convertToAccessor()}, "My first commit"+null)
     })
   }
 }
