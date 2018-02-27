57e0df86e06c7175706db726ed945af79e11635b
Zixuan Wang <wzixuanCC@gmail.com>
2018-02-27 13:02:19 -0500

    Event issues changed that modify the content values

--- globalFolder/route.js
+++ globalFolder/route.js
@@ -1,17 +1,13 @@
 {this.props.header}
 {this.convertToAccessor()}
 {this.props.footer}
-return(){
-  <InsertPosts/>
-}
-
 MainRouter.route('route, params', {
   actionTake(event){
     event.preventDefault()
     event.setup({
-        header: {this,props.header},
-        content: {this.convertToAccessor()},
-        footer: {this.props.footer}
+      header: {this.props.header}
+      content: {this.props.content}
+      footer: {this.props.footer}
     })
   }
 })
