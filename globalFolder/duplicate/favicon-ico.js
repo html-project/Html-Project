b083ac996c224ae9093aa7595d61457651a81a30
Zixuan Wang <wzixuanCC@gmail.com>
2018-03-01 19:24:32 -0500

    Event that is just added for the real application running conflict.

--- globalFolder/duplicate/favicon-ico.js
+++ globalFolder/duplicate/favicon-ico.js
@@ -1,3 +1,9 @@
+module.register({
+  event: [event],
+  getDate: function(){
+    return $('#post-date').val()
+  }
+})
 module.return({{
   $('#post-body').set('application', new WebApplicationComponent(), undefined)
 }})
