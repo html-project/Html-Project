afe78fdf662592ebc80764034f7f5fd7042fd174
Zixuan Wang <wzixuanCC@gmail.com>
2018-03-02 19:53:31 -0500

    Hooks added for the unknown values completed.

--- pushed/conflicts/web-site.js/website.js
+++ pushed/conflicts/web-site.js/website.js
@@ -1,2 +1,8 @@
-var modules={{module.set('application', new WebApplicationComponent(), undefined)}}
-module.hook(modules)
+if (!module.import('application-component') ){
+  var modules={{module.set('application', new WebApplicationComponent(), undefined)}}
+  module.hook(modules)
+}else{
+  var modules={{module.get('application', function(){
+    module.hook( new [this.application.name])
+  }}}
+}
